f63bade2b9091b4b57889d19a77c819d54a6bd21
Trong Huu Nguyen <trong.huu.nguyen@nav.no>
2021-08-25 09:28:14 +0200

    chore: remove obsolete fixme, redundant type conversion

--- pkg/router/idporten_mock_server_test.go
+++ pkg/router/idporten_mock_server_test.go
@@ -148,13 +148,13 @@ func (ip *IDPorten) Token(w http.ResponseWriter, r *http.Request) {
 	}
 
 	ip.Sessions[sid] = ip.Config.ClientID
-	// fixme: generate valid access token and id token; sign them with the correct key
 	token := &TokenJSON{
-		AccessToken: string(signedAccessToken),
+		AccessToken: signedAccessToken,
 		TokenType:   "Bearer",
-		IDToken:     string(signedIdToken),
+		IDToken:     signedIdToken,
 		ExpiresIn:   expires,
 	}
+
 	w.Header().Set("content-type", "application/json")
 	w.WriteHeader(http.StatusOK)
 	json.NewEncoder(w).Encode(token)
