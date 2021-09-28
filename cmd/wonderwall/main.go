214b14323c1877e7712aafa2724f0696a3fb955c
Morten Lied Johansen <morten.lied.johansen@nav.no>
2021-09-28 21:29:33 +0200

    Do graceful shutdown on signals

--- cmd/wonderwall/main.go
+++ cmd/wonderwall/main.go
@@ -4,9 +4,12 @@ import (
 	"context"
 	"encoding/base64"
 	"fmt"
+	"github.com/go-chi/chi"
 	"github.com/nais/wonderwall/pkg/metrics"
 	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
 	"time"
 
 	"github.com/go-redis/redis/v8"
@@ -103,17 +106,54 @@ func run() error {
 	go func() {
 		err := metrics.Handle(cfg.MetricsBindAddress)
 		if err != nil {
-			log.Errorf("fatal: metrics server error: %s", err)
-			os.Exit(1)
+			log.Fatalf("fatal: metrics server error: %s", err)
 		}
 	}()
-	return http.ListenAndServe(cfg.BindAddress, r)
+	return startServer(cfg, r)
+}
+
+func startServer(cfg *config.Config, r chi.Router) error {
+	server := http.Server{
+		Addr:    cfg.BindAddress,
+		Handler: r,
+	}
+
+	serverCtx, serverStopCtx := context.WithCancel(context.Background())
+
+	sig := make(chan os.Signal, 1)
+	signal.Notify(sig, syscall.SIGHUP, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT)
+	go func() {
+		<-sig
+
+		shutdownCtx, shutdownStopCtx := context.WithTimeout(serverCtx, 20*time.Second)
+
+		go func() {
+			<-shutdownCtx.Done()
+			if shutdownCtx.Err() == context.DeadlineExceeded {
+				log.Fatal("graceful shutdown timed out.. forcing exit.")
+			}
+		}()
+
+		err := server.Shutdown(shutdownCtx)
+		if err != nil {
+			log.Fatal(err)
+		}
+		shutdownStopCtx()
+		serverStopCtx()
+	}()
+
+	err := server.ListenAndServe()
+	if err != nil && err != http.ErrServerClosed {
+		return err
+	}
+
+	<-serverCtx.Done()
+	return nil
 }
 
 func main() {
 	err := run()
 	if err != nil {
-		log.Errorf("Fatal error: %s", err)
-		os.Exit(1)
+		log.Fatalf("Fatal error: %s", err)
 	}
 }
