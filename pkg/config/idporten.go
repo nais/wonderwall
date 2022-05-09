b3dfa547685f923f0f703016b3ca357596873f0f
Trong Huu Nguyen <trong.huu.nguyen@nav.no>
2022-05-09 11:49:44 +0200

    refactor: change default post-logout redirect uri for idporten

--- pkg/config/idporten.go
+++ pkg/config/idporten.go
@@ -9,7 +9,7 @@ func idportenFlags() {
 	viper.BindEnv(OpenIDClientJWK, "IDPORTEN_CLIENT_JWK")
 	viper.BindEnv(OpenIDWellKnownURL, "IDPORTEN_WELL_KNOWN_URL")
 
-	viper.SetDefault(OpenIDPostLogoutRedirectURI, "https://www.nav.no")
+	viper.SetDefault(OpenIDPostLogoutRedirectURI, "https://www.nav.no/no/utlogget")
 	viper.SetDefault(OpenIDACRValues, "Level4")
 	viper.SetDefault(OpenIDUILocales, "nb")
 }
